0daaa66378cd8bfdcd8de731050b19df1c024ee7
aymyo <hello.aymyo@gmail.com>
2024-02-02 17:31:21 +0100

    nice homepage

---
+++ src/content.ts
@@ -0,0 +1,45 @@
+export const tallers = [
+  {
+    title: 'Pinta una bossa de tela',
+    desc: 'Dues hores per connectar mentre expresses la teva creativitat pintant una bossa de tela amb pintura acrílica, ja sigui una nova, o donant-li una nova vida a aquella promocional que utilitzes per la compra.',
+    imgSrc: '/img/t-bossa.png',
+    imgAlt:
+      'Fotografia de dues participants del taller pintant bosses de tela amb pintura acrílica',
+  },
+  {
+    title: 'Pinta un mirallet',
+    desc: 'Podràs pintar un mirallet de 30x30 per penjar-lo i mirar-te cada matí i amb un toc de màgia!',
+    imgSrc: '/img/t-bossa-2.png',
+    imgAlt: "Fotografia d'una participant del taller pintant un mirallet",
+  },
+  {
+    title: 'Pinta un potet de vidre',
+    desc: 'Sembla que mai tenim prou pots de vidre acumulat a la vidra. Pinta’l per posar-hi espelmes, flors o llàpisos... Però sobretot, regala’t dues hores de tranquilitat i creativitat.',
+    imgSrc: '/img/t-potet.png',
+    imgAlt: 'Cartell de taller de pintar pots de vidre',
+  },
+  {
+    title: 'Pinta un coixí',
+    desc: 'Pinta un coixí de 40x40 amb pintura tèxtil. El coixí és de cotó i el pots rentar a màxima temperatura. Pots triar entre diferents dissenys o fer el teu propi disseny.',
+    imgSrc: '/img/t-coixi.png',
+    imgAlt: 'Cartell de taller de pintar coixins',
+  },
+  {
+    title: 'Pinta una samarreta',
+    desc: 'Pinta una samarreta de cotó amb pintura tèxtil. La samarreta és de cotó i el pots rentar a màxima temperatura. Pots triar entre diferents dissenys o fer el teu propi disseny.',
+    imgSrc: '/img/t-samarreta.png',
+    imgAlt: 'Cartell de taller de pintar samarretes',
+  },
+  {
+    title: 'Pinta una tassa',
+    desc: 'Pinta una tassa de ceràmica amb pintura especial per a aquest material. La tassa és apta per a rentavaixelles. Pots triar entre diferents dissenys o fer el teu propi disseny.',
+    imgSrc: '/img/t-tassa.png',
+    imgAlt: 'Cartell de taller de pintar tasses',
+  },
+  {
+    title: 'Pinta una ampolla de vidre',
+    desc: 'Pinta una ampolla de vidre amb pintura especial per a aquest material. La ampolla és apta per a rentavaixelles. Pots triar entre diferents dissenys o fer el teu propi disseny.',
+    imgSrc: '/img/t-ampolla.png',
+    imgAlt: 'Cartell de taller de pintar ampolles de vidre',
+  },
+]
