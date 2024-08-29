abaca63df21d4a1e6dd55bb69f9da2e2a59abc8c
aymyo <hello.aymyo@gmail.com>
2024-08-29 16:02:00 +0200

    naother try..

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,15 @@
 import { defineMiddleware } from 'astro:middleware'
 import { getLangFromUrl } from './utils'
 
-const isMaintenanceMode = true
+const isMaintenanceMode = import.meta.env.PUBLIC_MAINTENANCE_MODE === 'true'
 
 export const onRequest = defineMiddleware((context, next) => {
   const isMaintenancePath = context.url.pathname.endsWith('/manteniment')
-  const host = context.request.headers.get('host')
+ 
 
-  console.log('isMaintenanceMode', isMaintenanceMode)
-  console.log('context', context)
-
-  if (!isMaintenanceMode && isMaintenancePath) {
-    const lang = getLangFromUrl(context.url)
-    const redirectTo = lang === 'ca' ? '/' : `/${lang}/`
-    return context.redirect(redirectTo, 302);
-    return Response.redirect(`https://${host}/${redirectTo}`, 307)
-  } else if (isMaintenanceMode && !isMaintenancePath) {
+ if (isMaintenanceMode && !isMaintenancePath) {
     const lang = getLangFromUrl(context.url)
     const redirectTo = lang === 'ca' ? '/manteniment' : `/${lang}/manteniment`
     return context.redirect(redirectTo, 302);
-    return Response.redirect(`https://${host}/${redirectTo}`, 307)
-  }
+   }
 })
