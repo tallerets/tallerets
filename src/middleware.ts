2d5ac8cdf280c11deb613d606613aeb5fba835d8
aymyo <hello.aymyo@gmail.com>
2024-08-29 16:07:10 +0200

    ara si!

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,13 @@ const isMaintenanceMode = import.meta.env.PUBLIC_MAINTENANCE_MODE === 'true'
 export const onRequest = defineMiddleware((context, next) => {
   const isMaintenancePath = context.url.pathname.includes('manteniment')
  
-  console.log('path', context.url.pathname, isMaintenancePath)
-  if (isMaintenanceMode && !isMaintenancePath) {
+   if (isMaintenanceMode && !isMaintenancePath) {
     const lang = getLangFromUrl(context.url)
     const redirectTo = lang === 'ca' ? '/manteniment' : `/${lang}/manteniment`
+    return context.redirect(redirectTo, 302);
+  }  else if (!isMaintenanceMode && isMaintenancePath) {
+    const lang = getLangFromUrl(context.url)
+    const redirectTo = lang === 'ca' ? '/' : `/${lang}`
     return context.redirect(redirectTo, 302);
   }
 
