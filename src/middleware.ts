6f427dc06552170ec87f2f977ac646329ab03f4e
aymyo <hello.aymyo@gmail.com>
2024-08-29 14:12:48 +0200

    yasi

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,13 @@ import { getLangFromUrl } from './utils'
 
 export const onRequest = defineMiddleware((request, next) => {
   const isMaintenanceMode = import.meta.env.MAINTENANCE_MODE === 'true'
+  const isMaintenancePath = request.url.pathname.endsWith('/manteniment')
 
-  if (isMaintenanceMode && !request.url.pathname.endsWith('/manteniment')) {
+  if (!isMaintenanceMode && isMaintenancePath) {
+    const lang = getLangFromUrl(request.url)
+    const redirectTo = lang === 'ca' ? '/' : `/${lang}/`
+    return Response.redirect(new URL(redirectTo, request.url), 307)
+  } else if (isMaintenanceMode && !isMaintenancePath) {
     const lang = getLangFromUrl(request.url)
     const redirectTo = lang === 'ca' ? '/manteniment' : `/${lang}/manteniment`
     return Response.redirect(new URL(redirectTo, request.url), 307)
